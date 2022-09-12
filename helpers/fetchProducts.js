d8554921418692d21f98f62016e454df1b9e1a42
Giuliano <giulianovieiramartins@gmail.com>
2022-09-12 17:05:17 -0300

    agora sim, requisitos 1 e 2 prontos

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,15 +1,14 @@
 const fetchProducts = async (produto) => {
   if (!produto) {
-    throw new Error('You must provide an url')
+    throw new Error('You must provide an url');
   }
   try {
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
     const request = await fetch(url);
     const json = await request.json();
-    return json
-    
+    return json;
   } catch (error) {
-    return error.message
+    return error.message;
   }
 };
 
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,10 +8,14 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   it('verifica se fetchProducts é uma função', () =>{
     expect(typeof fetchProducts).toEqual('function')
-  });
-  it('verifica se a função, ao passar o parametro computador, a função fetch utiliza a URL correta', async ()=>{
+  })
+  it('verifica se a função, ao passar o parametro computador, a função fetch foi chamada', async ()=>{
     fetchProducts('computador')
     expect(fetch).toHaveBeenCalledTimes(1)
+  })
+  it('verifica se a função, ao passar o parametro computador, a função fetch utiliza a URL correta', async ()=>{
+    fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?q=computador`)
   })
   it('testa se ao receber o argumento computador retorna o objeto esperado computadorSearch', async () => {
     expect(await fetchProducts('computador')).toEqual(computadorSearch)
@@ -23,4 +27,4 @@ describe('1 - Teste a função fetchProducts', () => {
       expect(error.message).toBe('You must provide an url')
     }
   })
-});
+})
